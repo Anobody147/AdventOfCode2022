352f5886e96e9142b3653f102f4d812db39e94b3
Erikas <e.sokolovas@student.utwente.nl>
2022-12-14 13:30:52 +0100

    Solved day 13 part 1

--- solutions/day13.py
+++ solutions/day13.py
@@ -1,4 +1,11 @@
 from typing import List, Union
+from enum import Enum
+
+
+class Order(Enum):
+    ORDERED = 1
+    NOT_ORDERED = 2
+    UNKNOWN = 3
 
 def main():
     with open('../data/day13/day13.txt', 'r') as file:
@@ -14,7 +21,9 @@ def main():
         pack1 = eval(packet1)
         pack2 = eval(packet2)
 
-        print(pack1, pack2)
+        print(pack1)
+        print(pack2)
+        print()
 
         pairs[idx + 1] = (pack1, pack2)
 
@@ -24,8 +33,15 @@ def main():
         print(key)
         packet1, packet2 = pairs[key]
 
-        if ordered(packet1, packet2):
-            in_order.append(key)
+        packets_ordered = ordered(packet1, packet2)
+
+        match packets_ordered:
+            case Order.ORDERED:
+                in_order.append(key)
+            case Order.NOT_ORDERED:
+                pass
+            case Order.UNKNOWN:
+                in_order.append(key)
 
     print(in_order)
     print(sum(in_order))
@@ -33,16 +49,19 @@ def main():
     print()
 
 
-def ordered(left, right) -> bool:
+def ordered(left, right) -> Order:
     idx = 0
     while True:
+        if len(left) == idx == len(right):
+            return Order.UNKNOWN
+
         # if left list ran out first or both list ran out = ordered
         if idx == len(left):
-            return idx <= len(right)
+            return Order.ORDERED if idx <= len(right) else Order.NOT_ORDERED
 
         # if the right list ran out first = not ordered, if the same length = ordered
         if idx == len(right):
-            return idx == len(left)
+            return Order.ORDERED if idx == len(left) else Order.NOT_ORDERED
 
         # now check values
         left_type = type(left[idx])
@@ -51,13 +70,19 @@ def ordered(left, right) -> bool:
         if left_type == list and right_type == list:
             lists_ordered = ordered(left[idx], right[idx])
             # only if lists do not compare the same do we have an unordered list
-            if not lists_ordered:
-                return False
+            match lists_ordered:
+                case Order.ORDERED:
+                    return Order.ORDERED
+                case Order.NOT_ORDERED:
+                    return Order.NOT_ORDERED
+                case Order.UNKNOWN:
+                    pass
+
         elif left_type == int and right_type == int:
             if left[idx] < right[idx]:
-                return True
+                return Order.ORDERED
             elif left[idx] > right[idx]:
-                return False
+                return Order.NOT_ORDERED
             # if equal it tells us nothing
         else:
             # mixed types, so convert the int to a list
@@ -71,17 +96,15 @@ def ordered(left, right) -> bool:
                 right_element = [right_element]
 
             lists_ordered = ordered(left_element, right_element)
-            if not lists_ordered:
-                return False
+            match lists_ordered:
+                case Order.ORDERED:
+                    return Order.ORDERED
+                case Order.NOT_ORDERED:
+                    return Order.NOT_ORDERED
+                case Order.UNKNOWN:
+                    pass
 
         idx += 1
-
-
-
-
-
-
-
 
 
 if __name__ == '__main__':
